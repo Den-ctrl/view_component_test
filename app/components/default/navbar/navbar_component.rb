de23e0a4ecfca4145e659006d48250702e388ee7
Alden Nathan Go <aldengo99@gmail.com>
2025-01-30 11:53:52 +0800

    initial commit

---
+++ app/components/default/navbar/navbar_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Default::Navbar::NavbarComponent < ViewComponent::Base
+end
---
+++ test/components/default/navbar/navbar_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Default::Navbar::NavbarComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Default::Navbar::NavbarComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
