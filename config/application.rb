50f7821cb1256d8493d14aa02ee7be8f5e80b4cc
Alden Nathan Go <aldengo99@gmail.com>
2025-01-31 09:10:59 +0800

    view component tailwind config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ViewComponentTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-    config.autoload_paths += %W[#{config.root}/app/components]
+    # config.autoload_paths += %W[#{config.root}/app/components]
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
